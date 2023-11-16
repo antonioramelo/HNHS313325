0ea766c1b8e8439e1fddef7c5c2019c8be243cc6
antonioramelo <150996933+antonioramelo@users.noreply.github.com>
2023-11-16 13:46:17 +0800

    Create SwitchingTables.js

---
+++ SwitchingTables.js
@@ -0,0 +1,18 @@
+<script>
+  function changeTable() {
+    // Get the selected value from the dropdown
+    var selectedTable = document.getElementById("table-selector").value;
+
+    // Hide all tables
+    var tables = document.querySelectorAll("#table-container table");
+    tables.forEach(function(table) {
+      table.style.display = "none";
+    });
+
+    // Show the selected table
+    var selectedTableElement = document.getElementById(selectedTable);
+    if (selectedTableElement) {
+      selectedTableElement.style.display = "table";
+    }
+  }
+</script>
